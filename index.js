b7e17d3ccc7399b62339a4e7af60cfdf47cf1c7d
Walker <jwalkerlyle@gmail.com>
2016-08-04 20:48:24 -0500

    express yourself skeleton

---
+++ index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Set our views directory
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', { names:['Abe', 'George', 'Sue']});
+});
+
+const server = http.createServer(app);
+const port = 3000;
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});

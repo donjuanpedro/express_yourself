65a0e5cde831e9190af5a59a6615c5c75f4e9958
Walker <jwalkerlyle@gmail.com>
2016-08-09 17:47:07 -0500

    working and corrected

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const http = require('http');
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
+const ghAvatar = require('gh-avatar');
 
 const app = express();
+
+
+app.use(express.static(__dirname + '/public'));
 
 // Set our views directory
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', { name:'Walker'});
+app.get('/', (req, res, next) => {
+  ghAvatar('donjuanpedro').then(avatar => {
+  res.render('index.ejs', {
+    ghAvatar: avatar,
+    name:'Walker',
+    skills: ['HTML', 'CSS', 'Javascript', 'Express.js'],
+    hobbies: ['Reading', 'Camping', 'Cooking', 'Coding']
+    });
+  });
 });
 
 console.log(__dirname);
